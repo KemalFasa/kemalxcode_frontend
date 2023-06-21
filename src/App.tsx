ad82bb7fc5bfd2cb469f5c31403b568d18d532cf
apple <fasa.kemal@gmail.com>
2023-06-21 15:46:20 +0700

    ganti logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p style={{ color: "rgb(2, 208, 240" }}>
+          {/* Edit <code>src/App.tsx</code> and save to reload. */}
+          Kemal X Code
         </p>
-        <a
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p style={{ color: "rgb(2, 208, 240" }}>
+          awww Create Future Form Future
+        </p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
